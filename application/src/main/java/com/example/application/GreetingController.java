c9275b97727cf713c7f352a5bdaa2474c6ba6fb3
jossuehernandez <jossuehernandez@gluo.mx>
2019-04-21 15:40:05 -0500

    Update GreetingController.java

--- application/src/main/java/com/example/application/GreetingController.java
+++ application/src/main/java/com/example/application/GreetingController.java
@@ -10,7 +10,8 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-
+    
+    // @Annotations
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
